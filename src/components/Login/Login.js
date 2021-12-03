900f23c4f2a50d3c437219603aac1fa352c2be5b
gingercook <vahitaras@gmail.com>
2021-12-03 00:37:29 -0500

    debouncing comcept introduced

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,19 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    // Debouncing in Javascript is an exercise to enhance browser performance 
+    // during any time-consuming computations. 
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
